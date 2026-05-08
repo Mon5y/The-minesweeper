b1a59093290ac7f49070d60b9985f2a49edda2f4
Monsy <fhhdalex@gmail.com>
2026-05-08 21:38:38 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,37 @@
 #include <iostream>
 #include <SFML/Graphics.hpp>
 
+
+class Cell
+{
+    public:
+        enum State{Hidden,Opened,Flagged};
+    private:
+        bool isMine;
+        int NeighborMines;
+        State state;
+    public:
+        
+    Cell():isMine(false),NeighborMines(0),state(Hidden) {}
+        
+        void setMine() { isMine = true;}
+        bool hasMine() const {return isMine;}
+
+        void setNeighborMine(int count) {NeighborMines = count;}
+        int  hasNeighborMine() const {return NeighborMines;}
+
+        State getState() const {return state;}
+        void getState(State s) {state = s;}
+
+        void open() {if(state != Flagged) state = Opened;}
+        void toggleFlag()
+        {
+            if(state == Hidden) state = Flagged;
+            else if (state == Flagged) state == Hidden;
+        }
+    
+};
+
 int main()
 {
     sf::RenderWindow window(sf::VideoMode{1600,900},"minesweper");
@@ -15,6 +46,7 @@ int main()
                 window.close();
             }
         }
+        window.setFramerateLimit(60);
         window.clear();
         window.display();
     }
