704744e15f42a171e6130512c90617bd7d557f26
Monsy <fhhdalex@gmail.com>
2026-06-17 22:19:03 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,53 +2,58 @@
 #include <SFML/Graphics.hpp>
 
 
+
 class Cell
 {
-    public:
-        enum State{Hidden,Opened,Flagged};
-    private:
-        bool isMine;
-        int NeighborMines;
-        State state;
-    public:
-        
-    Cell():isMine(false),NeighborMines(0),state(Hidden) {}
-        
-        void setMine() { isMine = true;}
-        bool hasMine() const {return isMine;}
+public:
+    enum State{Hidden,Opened,Flagged};    
+private:
+    bool isMine;
+    int NeightboorMines;
+    State state;
+public:
+    Cell() : isMine(false),NeightboorMines(0),state(Hidden) {}
 
-        void setNeighborMine(int count) {NeighborMines = count;}
-        int  hasNeighborMine() const {return NeighborMines;}
+    void setMine() {isMine = true;}
+    bool hasMine() const {return isMine;}
 
-        State getState() const {return state;}
-        void getState(State s) {state = s;}
+    void setNeightbourMines(int  count) { NeightboorMines = count;}
+    int getNeightbour() const {return NeightboorMines;}
 
-        void open() {if(state != Flagged) state = Opened;}
-        void toggleFlag()
-        {
-            if(state == Hidden) state = Flagged;
-            else if (state == Flagged) state == Hidden;
-        }
-    
+    State getState() const{return state;}
+    void setState(State s) {state = s;}
+
+    void open() {if (state != Flagged) state = Opened;}
+    void ToggleFlag()
+    {
+        if(state == Hidden) state = Flagged;
+        else if(state == Flagged) state = Hidden;
+    }
 };
+
+class GameBoard
+{
+
+};
+
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode{1600,900},"minesweper");
+    const int weight = 1600;
+    const int hight = 900;
+    sf::RenderWindow window({weight,hight},"minesweeper");
 
     while(window.isOpen())
     {
         sf::Event event;
-        while(window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
+        while(window.pollEvent(event)){
+            if(event.type == sf::Event::Closed)
             {
                 window.close();
             }
         }
-        window.setFramerateLimit(60);
         window.clear();
         window.display();
     }
-    return 0;
+
 }
