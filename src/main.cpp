64d80feabf440a16c2d63145e54eee07673829db
Monsy <fhhdalex@gmail.com>
2026-06-17 22:43:05 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,25 @@
 #include <iostream>
 #include <SFML/Graphics.hpp>
-
+#include <cstdint>
+#include <vector>
 
 
 class Cell
 {
 public:
-    enum State{Hidden,Opened,Flagged};    
+    enum  State{Hidden,Opened,Flagged};    
 private:
     bool isMine;
-    int NeightboorMines;
+    int NeighborMines;
     State state;
 public:
-    Cell() : isMine(false),NeightboorMines(0),state(Hidden) {}
+    Cell() : isMine(false),NeighborMines(0),state(Hidden) {}
 
     void setMine() {isMine = true;}
     bool hasMine() const {return isMine;}
 
-    void setNeightbourMines(int  count) { NeightboorMines = count;}
-    int getNeightbour() const {return NeightboorMines;}
+    void setNeighborMines(int  count) { NeighborMines = count;}
+    int getNeightbourCount() const {return NeighborMines;}
 
     State getState() const{return state;}
     void setState(State s) {state = s;}
@@ -33,7 +34,16 @@ public:
 
 class GameBoard
 {
+private:
+    int Wight;
+    int Height;
 
+    std::vector<std::vector<Cell>> board;
+    board.resize(Height);
+    for (int i = 0;i<Wight;++i)
+    {
+        board[i].resize(Wight);
+    }
 };
 
 
