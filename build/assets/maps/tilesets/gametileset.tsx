6b6ddd2b21bc8558808daa7aafdaa3480ab2ef87
Monsy <fhhdalex@gmail.com>
2026-07-05 17:27:39 +0200

    Git: ignore build directory

--- build/assets/maps/tilesets/gametileset.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="gametileset" tilewidth="16" tileheight="16" tilecount="5777" columns="109">
- <image source="../../tilesets/tileset.png" trans="000000" width="1744" height="848"/>
-</tileset>
